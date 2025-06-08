c167d6c55f34435ad2140e9c9319159c205b3c44
ibitabadger <126259117+ibitabadger@users.noreply.github.com>
2025-06-08 13:55:56 -0500

    pprof

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,13 +5,19 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
-
 	"performance-analysis-api/internal/router"
 )
 
 func main() {
+
+	go func() {
+		fmt.Println("pprof disponible en http://localhost:6060/debug/pprof/")
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	r := router.NewRouter()
 
 	fmt.Println("Servidor escuchando en el puerto 8080...")
 	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
